59763038ffccbd75feb009519dff4a264683e505
bunakov.ev <egor.bunakov2003@gmail.com>
2025-04-28 21:30:14 +0300

    пытаемся поднять sd карту-безрезультатно FR_NOT_FILESYSTEM FR_NOT_READY

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -216,7 +216,35 @@ int main(void)
   }
 
   radio_init(&radio);
-  SD_init();
+  //SD_init();
+
+  //Open the file system
+  //f_mount(NULL, "", 0);
+  fres = f_mount(&FatFs, "", 1); //1=mount now
+	while(fres != FR_OK){
+		fres = f_mount(&FatFs, "", 1); //1=mount now
+		myprintf("f_mount error (%i)\r\n", fres);
+		HAL_Delay(100);
+  }
+
+  //Now let's try and write a file "write.txt"
+  fres = f_open(&fil, "write.txt", FA_WRITE | FA_CREATE_ALWAYS);
+  if(fres == FR_OK) {
+	myprintf("I was able to open 'write.txt' for writing\r\n");
+  } else {
+	myprintf("f_open error (%i)\r\n", fres);
+  }
+
+  //Copy in a string
+  snprintf((char*)readBuf, strlen((char*)readBuf), "Start work with SD card!\r\n");
+
+  fres = f_write(&fil, readBuf, strlen((char*)readBuf), &bytesWrote);
+  if(fres != FR_OK) {
+	myprintf("f_write error (%i)\r\n");
+  }
+
+  //Be a tidy kiwi - don't forget to close your file!
+  f_close(&fil);
 
   HAL_TIM_Base_Start_IT(&htim3);
 
